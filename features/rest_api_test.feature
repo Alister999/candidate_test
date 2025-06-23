8994b26b364bb8c0062cb105b61fb6e2b1ab7ba9
Aleksandr Sidorov <a.sidorov@teleguard.com>
2025-06-23 14:49:38 +0200

    add put and check steps and put test

--- features/rest_api_test.feature
+++ features/rest_api_test.feature
@@ -2,7 +2,23 @@
 
 Feature: REST API Testing
 
-  Scenario: Retrieve user list
+  Scenario: Adding new user to userlist
+    When добавляю пользователя с параметрами:
+      | login     | unique.user    |
+      | name      | Alister         |
+      | surname   | Lavey          |
+      | password  | Pass123!     |
+    When получаю информацию о пользователях
+    Then проверяю наличие логина unique.user в списке пользователей
+    When Проверяю соответствие данных пользователя с логином unique.user эталону:
+      | login     | unique.user    |
+      | name      | Alister         |
+      | surname   | Lavey          |
+      | password  | Pass123!     |
+      | active    | 1            |
+    When удаляю пользователя с логином unique.user
+
+  Scenario: Delete user from user list
     When добавляю пользователя с параметрами:
       | login     | unique.user    |
       | name      | Alister         |
@@ -13,3 +29,26 @@ Feature: REST API Testing
     When удаляю пользователя с логином unique.user
     When получаю информацию о пользователях
     Then проверяю отсутствие логина unique.user в списке пользователей
+
+
+  Scenario: Change user in userlist
+    When добавляю пользователя с параметрами:
+      | login     | unique.user    |
+      | name      | Alister         |
+      | surname   | Lavey          |
+      | password  | Pass123!     |
+    When получаю информацию о пользователях
+    Then проверяю наличие логина unique.user в списке пользователей
+    When меняю параметры пользователя с логином unique.user:
+      | login     | unique.user1    |
+      | name      | Alister2         |
+      | surname   | Lavey3          |
+      | password  | Pass1234     |
+      | active    | 2            |
+    When Проверяю соответствие данных пользователя с логином unique.user1 эталону:
+      | login     | unique.user1    |
+      | name      | Alister2         |
+      | surname   | Lavey3          |
+      | password  | Pass1234     |
+      | active    | 2            |
+    When удаляю пользователя с логином unique.user1
--- features/step_definitions/rest_tests_steps.rb
+++ features/step_definitions/rest_tests_steps.rb
@@ -56,6 +56,7 @@ When(/^нахожу пользователя с логином (\w+\.\w+)$/) do 
   $logger.info("Найден пользователь #{login} с id:#{@scenario_data.users_id[login]}")
 end
 
+
 When(/^удаляю пользователя с логином (\w+\.\w+)$/) do |login|
   step %(нахожу пользователя с логином #{login})
   deleting_user_id = @scenario_data.users_id[login]
@@ -65,5 +66,52 @@ When(/^удаляю пользователя с логином (\w+\.\w+)$/) do 
     $logger.info("Пользователь с логином #{login} успешно удален")
   else
     raise "Не удалось удалить пользователя с логином #{login}, код ответа: #{response.code}"
+  end
+end
+
+When(/^Проверяю соответствие данных пользователя с логином (\w+\.\w+) эталону:$/) do |login_old, data_table|
+  user_data = data_table.raw
+
+  login = user_data[0][1]
+  name = user_data[1][1]
+  surname = user_data[2][1]
+  password = user_data[3][1]
+  active = user_data[4][1].to_i
+
+  @scenario_data.users_full_info.select do |user|
+    if user['login'] == login
+      $logger.info("Проверка пользователя с логином #{login} на соответствие эталону")
+      expect(user['name']).to eq(name)
+      expect(user['surname']).to eq(surname)
+      expect(user['active']).to eq(active.to_i)
+    end
+  end
+end
+
+When(/^меняю параметры пользователя с логином (\w+\.\w+):$/) do |login_old, data_table|
+  user_data = data_table.raw
+
+  login = user_data[0][1]
+  name = user_data[1][1]
+  surname = user_data[2][1]
+  password = user_data[3][1]
+  active = user_data[4][1].to_i
+
+  $logger.info("меняю пользователя с логином #{login_old} на пользователя c логином #{login} именем #{name} фамилией #{surname} паролем #{password} и активностью #{active}")
+  
+  step %(нахожу пользователя с логином #{login_old})
+  changing_user_id = @scenario_data.users_id[login_old]
+
+  response = $rest_wrap.put("/users/#{changing_user_id}", login: login,
+                                       name: name,
+                                       surname: surname,
+                                       password: password,
+                                       active: active)
+  $logger.info(response.inspect)
+  
+  if response['status'] == 200
+    $logger.info("Пользователь с логином #{login_old} успешно изменен")
+  else
+    raise "Не удалось изменить пользователя с логином #{login_old}, код ответа: #{response['status']}"
   end
 end
